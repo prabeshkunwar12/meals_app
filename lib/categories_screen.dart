53beb0a7a1782002da9865b2f84d34dcb29ae5ab
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-06-15 22:19:56 -0300

    defined category model and screen

---
+++ lib/categories_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category:'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [],
+      ),
+    );
+  }
+}
