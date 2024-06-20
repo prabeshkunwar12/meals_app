af36b2b39b994e6fbeb5cefebe12f7f7c0ff2400
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-06-20 00:07:53 -0300

    made favorite button icon change if favorited

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -11,6 +11,9 @@ class MealDetailsScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -29,7 +32,7 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: const Icon(Icons.star),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           )
         ],
       ),
