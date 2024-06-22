3bbf31f5315c86c5390dfb632d00210773714f07
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-06-22 11:24:48 -0300

    added cross screen animation between meal item and meal details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -51,14 +51,17 @@ class MealDetailsScreen extends ConsumerWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(
-                meal.imageUrl,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(
+                  meal.imageUrl,
+                ),
+                fit: BoxFit.cover,
+                height: 250,
+                width: double.infinity,
               ),
-              fit: BoxFit.cover,
-              height: 250,
-              width: double.infinity,
             ),
             const SizedBox(
               height: 14,
--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -21,12 +21,15 @@ class MealItem extends StatelessWidget {
         onTap: onMealTap,
         child: Stack(
           children: [
-            FadeInImage(
-              placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl),
-              fit: BoxFit.cover,
-              height: 200,
-              width: double.infinity,
+            Hero(
+              tag: meal.id,
+              child: FadeInImage(
+                placeholder: MemoryImage(kTransparentImage),
+                image: NetworkImage(meal.imageUrl),
+                fit: BoxFit.cover,
+                height: 200,
+                width: double.infinity,
+              ),
             ),
             Positioned(
               bottom: 0,
